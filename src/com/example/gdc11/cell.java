7283b39be963b44536501695c0151655f42773f6
t09h3r <chris.halden@gmail.com>
2012-10-26 16:08:25 +0100

    ok

--- src/com/example/gdc11/cell.java
+++ src/com/example/gdc11/cell.java
@@ -3,6 +3,11 @@ package com.example.gdc11;
 public class cell{
   private float pos[]=new float[4];
   public void Render(){
+GLfloat vertices[] = {1,0,0, 0,1,0, -1,0,0};
+GLES20.glEnableClientState(GL_VERTEX_ARRAY);
+GLES20.glVertexPointer(3, GL_FLOAT, 0, vertices);
+GLES20.glDrawArrays(GL_TRIANGLES, 0, 3);
+GLES20.glDisableClientState(GL_VERTEX_ARRAY);
   }
   public cell(float p[]){
     pos[0]=p[0];
