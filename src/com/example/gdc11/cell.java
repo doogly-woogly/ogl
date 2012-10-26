1d2fe2056b6dcb53614917e8c72a8fdb924c6c40
t09h3r <chris.halden@gmail.com>
2012-10-26 16:11:14 +0100

    ok

--- src/com/example/gdc11/cell.java
+++ src/com/example/gdc11/cell.java
@@ -5,11 +5,11 @@ import android.opengl.*;
 public class cell{
   private float pos[]=new float[4];
   public void Render(){
-GLfloat vertices[] = {1,0,0, 0,1,0, -1,0,0};
-GLES20.glEnableClientState(GL_VERTEX_ARRAY);
-GLES20.glVertexPointer(3, GL_FLOAT, 0, vertices);
-GLES20.glDrawArrays(GL_TRIANGLES, 0, 3);
-GLES20.glDisableClientState(GL_VERTEX_ARRAY);
+GLES20.GLfloat vertices[] = {1,0,0, 0,1,0, -1,0,0};
+GLES20.glEnableClientState(GLES20.GL_VERTEX_ARRAY);
+GLES20.glVertexPointer(3, GLES20.GL_FLOAT, 0, vertices);
+GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+GLES20.glDisableClientState(GLES20.GL_VERTEX_ARRAY);
   }
   public cell(float p[]){
     pos[0]=p[0];
