ef022a8be9dd4b9f72d330dc65437617e0b8cc28
t09h3r <chris.halden@gmail.com>
2012-10-26 15:01:35 +0100

    ok

--- src/com/example/gdc11/GDC11Activity.java
+++ src/com/example/gdc11/GDC11Activity.java
@@ -321,6 +321,11 @@ tV[2]=0.5f;//near clip
 //point a=0,0,0
 //point b=press
 Matrix.multiplyMV(tR,0,rotation,0,tV,0);
+float l=Math.sqrt(tR[0]*tR[0]+tR[1]*tR[1]+tR[2]*tR[2]);
+tR[0]/=l;
+tR[1]/=l;
+tR[2]/=l;
+
 tR[0]*=10;
 tR[1]*=10;
 tR[2]*=10;
