830ab92608eeff4f4e8efc1ad1d313ad2bba94cd
t09h3r <chris.halden@gmail.com>
2012-10-26 16:29:53 +0100

    ok

--- src/com/example/gdc11/cell.java
+++ src/com/example/gdc11/cell.java
@@ -4,17 +4,45 @@ import android.opengl.*;
 
 public class cell{
   private float pos[]=new float[4];
+
+// New class members
+/** Store our model data in a float buffer. */
+private final FloatBuffer mTriangle1Vertices;
+private final FloatBuffer mTriangle2Vertices;
+private final FloatBuffer mTriangle3Vertices;
+ 
+/** How many bytes per float. */
+private final int mBytesPerFloat = 4;
+ 
+
   public void Render(){
-GLES20.GLfloat vertices[] = {1,0,0, 0,1,0, -1,0,0};
-GLES20.glEnableClientState(GLES20.GL_VERTEX_ARRAY);
-GLES20.glVertexPointer(3, GLES20.GL_FLOAT, 0, vertices);
-GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
-GLES20.glDisableClientState(GLES20.GL_VERTEX_ARRAY);
   }
   public cell(float p[]){
     pos[0]=p[0];
     pos[1]=p[1];
     pos[2]=p[2];
   //  pos[3]=p[3];
+
+    // This triangle is red, green, and blue.
+    final float[] triangle1VerticesData = {
+            // X, Y, Z,
+            // R, G, B, A
+            -0.5f, -0.25f, 0.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+ 
+            0.5f, -0.25f, 0.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+ 
+            0.0f, 0.559016994f, 0.0f,
+            0.0f, 1.0f, 0.0f, 1.0f};
+ 
+ 
+    // Initialize the buffers.
+    mTriangle1Vertices = ByteBuffer.allocateDirect(triangle1VerticesData.length * mBytesPerFloat)
+    .order(ByteOrder.nativeOrder()).asFloatBuffer();
+ 
+ 
+    mTriangle1Vertices.put(triangle1VerticesData).position(0);
+
   }
 }
