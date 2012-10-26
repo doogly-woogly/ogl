c46466162738ab18ea9a9972130050a29bb86acd
t09h3r <chris.halden@gmail.com>
2012-10-26 18:10:18 +0100

    ok

--- src/com/example/gdc11/cell.java
+++ src/com/example/gdc11/cell.java
@@ -16,5 +16,22 @@ glDisableClientState(GL_VERTEX_ARRAY);
     pos[1]=p[1];
     pos[2]=p[2];
   //  pos[3]=p[3];
+
+    float triangleCoords[] = {
+        // X, Y, Z
+        -0.5f, -0.25f, 0,
+         0.5f, -0.25f, 0,
+         0.0f,  0.559016994f, 0,
+         0.0f,  0f, 0
+    }; 
+
+    // initialize vertex Buffer for triangle  
+    ByteBuffer vbb = ByteBuffer.allocateDirect(
+            // (# of coordinate values * 4 bytes per float)
+            triangleCoords.length * 4); 
+    vbb.order(ByteOrder.nativeOrder());// use the device hardware's native byte order
+    triangleVB = vbb.asFloatBuffer();  // create a floating point buffer from the ByteBuffer
+    triangleVB.put(triangleCoords);    // add the coordinates to the FloatBuffer
+    triangleVB.position(0);   
   }
 }
