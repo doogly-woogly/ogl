bbd1bfc8cf22a3c5b9d9b28f6641b86fe30f9b80
t09h3r <chris.halden@gmail.com>
2012-10-26 13:30:02 +0100

    ok

--- src/com/example/gdc11/GDC11Activity.java
+++ src/com/example/gdc11/GDC11Activity.java
@@ -452,7 +452,7 @@ mViewMatrix[15]=1;
         // Called from the UI when the user zooms the scene.
         public void zoom(float z) {
             //mZ = (float) Math.min(5, Math.max(mZ - z / 300, 1.6));
-            mFOV+=z;
+            mFOV+=z/300;
 perspectiveM(
                     mProjectionMatrix,
                     (float)Math.toRadians(mFOV),
